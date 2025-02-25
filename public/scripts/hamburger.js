30c2b3f7ab7b04a6e7c784daed9c1dbab6b4718f
vijay <bhaskarv819@gmail.com>
2025-02-25 13:21:02 +0530

    proceesing

--- public/scripts/hamburger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
- const menuIcon = document.querySelector(".menu-icon");
- const navbarMenu = document.querySelector(".navbar-menu");
-
- menuIcon.addEventListener("click", function () {
-   navbarMenu.style.display = navbarMenu.style.display === "flex" ? "none" : "flex";
- });
-});
